65bd412b5e55aeec951fafb13d00375aed3191a2
Vladik <vlad.a.aleynikov@gmail.com>
2023-10-03 20:45:44 +0300

    add footer adaptive

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const burger = document.getElementById("header__burger");
 const nav = document.getElementById("header__nav");
 const tel = document.getElementById("header__tel");
 const header__bg = document.getElementById("header__modal-bg");
+const body = document.body;
 
 const toggleHeader = (e) => {
       burger.classList.toggle("active");
       nav.classList.toggle("active");
       tel.classList.toggle("active");
       header__bg.classList.toggle("active");
-      
+      body.classList.toggle("hide")
 }
 burger.addEventListener("click",  toggleHeader)
 header__bg.addEventListener("click",  toggleHeader)
