7cab31b8518918733a4db00e10c19afb376efa65
Vladik <vlad.a.aleynikov@gmail.com>
2023-10-03 12:25:19 +0300

    add header burger

---
+++ index.js
@@ -0,0 +1,14 @@
+const burger = document.getElementById("header__burger");
+const nav = document.getElementById("header__nav");
+const tel = document.getElementById("header__tel");
+const header__bg = document.getElementById("header__modal-bg");
+
+const toggleHeader = (e) => {
+      burger.classList.toggle("active");
+      nav.classList.toggle("active");
+      tel.classList.toggle("active");
+      header__bg.classList.toggle("active");
+      
+}
+burger.addEventListener("click",  toggleHeader)
+header__bg.addEventListener("click",  toggleHeader)
